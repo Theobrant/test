8b4554418dcaea45ec50c993a65017d5d181417f
theobrant <fedotoskk@gmain.com>
2024-07-23 12:41:01 +0300

    123

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ HighAndLow("1 2 3 4 5")  // return "5 1"
 HighAndLow("1 2 -3 4 5") // return "5 -3"
 HighAndLow("1 9 3 4 -5") // return "9 -5"
 */
+
 func HighAndLow(in string) string {
 
 	return "throw towel"
